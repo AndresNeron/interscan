efe240110bbc15355628e29aaed57c70fdb25fa6
NeronNymus <jorge.gonzalez@cimat.mx>
2025-10-28 12:55:42 -0600

    update

--- http_fuzzer/install_fuzzer.sh
+++ http_fuzzer/install_fuzzer.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+# This script creates the virtual environment to fuzz port 80
+# of discovered IPs
+
+set -e
+
+# Required system packages
+REQUIRED_PACKAGES=(python3-dev python3-virtualenv)
+
+echo "[+] Installing required packages..."
+sudo apt update || true
+sudo apt install -y "${REQUIRED_PACKAGES[@]}"
+
+# Define paths
+rootPath="$(dirname "$(realpath "$0")")"
+envPath="$rootPath/crawlEnv"
+
+
+# Create virtual environment
+echo "[+] Creating virtual environment..."
+virtualenv "$envPath" --python=python3
+
+# Activate and install Python dependencies
+echo "[+] Installing Python dependencies..."
+source "$envPath/bin/activate"
+pip install -r requirements_cpu.txt
+deactivate
 
